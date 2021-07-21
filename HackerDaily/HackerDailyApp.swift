63f0c707b1424bd79f691701991e24432b41d211
Hamletdev <hamletdev001@gmail.com>
2021-07-21 09:40:21 +0530

    Initial Commit

---
+++ HackerDaily/HackerDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerDailyApp.swift
+//  HackerDaily
+//
+//  Created by Amit Chaudhary on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
